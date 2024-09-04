b6d44396432f89eedea412b0f21fff79c6f95769
next2523 <jordan.dean161@gmail.com>
2024-09-04 11:51:21 -0400

    Finished Positional Encoding

--- CTEN/encoder/ff.py
+++ CTEN/encoder/ff.py
@@ -1,4 +1,6 @@
 import torch.nn as nn
+import torch
+import math
 from additional import activation
 
 class EncoderFeedForwardNetwork(nn.Module):
@@ -13,3 +15,24 @@ class EncoderFeedForwardNetwork(nn.Module):
             self.relu(
                 self.recurrent1(x)
                 ))
+    
+class PositionalEncoding(nn.Module):
+    # Creates Class to Input Position Information into Each Token
+    def __init__(self, model, max_sequence_length):
+        super(PositionalEncoding, self).__init__()
+
+        # Creates Tensors for Positional Encodings
+        encoding = torch.zeros(max_sequence_length, model)
+        position = torch.arange(0, max_sequence_length, dtype=torch.float).unsqueeze(1)
+        divided_term = torch.exp(torch.arange(0, model, 2).float() * -(math.log(10000.0) / model))
+
+        # Uses Sine and Cosine Functions for Position Encoding
+        encoding[:, 0::2] = torch.sin(position * divided_term)
+        encoding[:, 1::2] = torch.cos(position * divided_term)
+
+        # Creates Register Buffers for Unsqueezing Tensors
+        self.register_buffer('encoding', encoding.unsqueeze(0))
+
+    def forward(self, x):
+        # Injects Positional Encodings
+        return x + self.encoding[:, :x.size(1)]
