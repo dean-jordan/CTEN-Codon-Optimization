7fdacf206ad1ff94d953168962c467acc71ef6d6
next2523 <jordan.dean161@gmail.com>
2024-09-04 17:48:36 -0400

    Finalized Decoder

--- CTEN/decoder/attention_d.py
+++ CTEN/decoder/attention_d.py
@@ -1,7 +1,60 @@
 import torch
 import torch.nn as nn
-import torch.optim as optim
-import torch.utils.data as data
 import math
-import copy
+from additional import activation
 
+class DecoderAttention(nn.Module):
+    def __init__(self, model, num_heads):
+        super(DecoderAttention, self).__init__()
+        # Model Dimension Must be Divisible By Number of Attention Heads
+        assert model % num_heads == 0
+
+        # Create Dimensions for Model and Attention Heads
+        self.model = model
+        self.num_heads = num_heads
+        self.d_k = model // num_heads
+
+        # Initial set of Recurrent Layers for Input Transformation
+        self.query = nn.Linear(model, model)
+        self.key = nn.Linear(model, model)
+        self.value = nn.Linear(model, model)
+        self.output = nn.Linear(model, model)
+
+    def dot_product_attention(self, Q, K, V, mask=None):
+        # Find Attention Values
+        attention_scores = torch.matmul(Q, K.transpose(-2, -1)) / math.sqrt(self.d_k)
+
+        # Masking to Prevent Attention to Non-Trainable Layers
+        if mask is not None:
+            attention_scores = attention_scores.masked_fill(mask == 0, -1e9)
+
+        # Softmax is Used at End of Attention Mechanism
+        attention_probabilities = activation.Softmax(x=attention_scores, dim=-1)
+
+        # Normalization of Softmax Values
+        output = torch.matmul(attention_probabilities, V)
+
+        return output
+
+    def combine_heads(self, x):
+        # Re-Combines Attention to Original Shape
+        batch_size, sequence_length, d_k = x.size()
+        return x.view(batch_size, sequence_length, d_k, self.num_heads).transpose(1, 2)
+    
+    def split_heads(self, x):
+        # Changing Inputs to num_heads for Multi-Head Attention
+        batch_size, _, sequence_length, d_k = x.size()
+        return x.transpose(1, 2).contiguous().view(batch_size, sequence_length, self.model)
+    
+    def forward(self, Q, K, V, mask=None):
+        # Splitting Heads to Complete Attention Mechanism
+        # Applying Linear Transformations to Input
+        Q = self.split_heads(self.query(Q))
+        K = self.split_heads(self.key(K))
+        V = self.split_heads(self.value(V))
+
+        # Scaled Dot-Product Attention
+        attention_output = self.dot_product_attention(Q, K, V, mask)
+
+        # Heads are Combined and Output Transformations Applied
+        output = self.output(self.combine_heads(attention_output))
--- CTEN/decoder/decoder.py
+++ CTEN/decoder/decoder.py
@@ -1,7 +1,39 @@
-import torch
 import torch.nn as nn
-import torch.optim as optim
-import torch.utils.data as data
-import math
-import copy
+import attention_d
+import ff_d
 
+class DecoderModule(nn.Module):
+    # Initializing the Decoder Module
+    def __init__(self, model, num_heads, ff, dropout):
+        super(DecoderModule, self).__init__()
+        
+        # Utilizing Decoder Attention Mechanism with Multi-Head and Cross Attention
+        self.attention1 = attention_d.DecoderAttention(model, num_heads)
+        self.attention2 = attention_d.DecoderAttention(model, num_heads)
+
+        # Implementing Feed-Forward Network
+        self.feed_forward = ff_d.DecoderFeedForwardNetwork(model, ff)
+
+        # Implementing Layer Normalization-Based Regularization
+        self.normalization1 = nn.LayerNorm(model)
+        self.normalization2 = nn.LayerNorm(model)
+        self.normalization3 = nn.LayerNorm(model)
+
+        # Implementing Dropout-Based Regularization
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, encoder_output, source_mask, target_mask):
+        # Multi-Head Attention Output
+        attention_output = self.attention1(x, x, x, target_mask)
+        x = self.normalization1(x + self.dropout(attention_output))
+
+        # Cross-Attention Output
+        attention_output = self.attention2(x, encoder_output, encoder_output, source_mask)
+        x = self.normalization2(x + self.dropout(attention_output))
+
+        # Feed-Forward Network Output
+        feed_forward_output = self.feed_forward(x)
+        x = self.normalization3(x + self.dropout(feed_forward_output))
+
+        # Outputting Regularized Decoder Values
+        return x
